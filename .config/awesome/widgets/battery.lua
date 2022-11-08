2d8af5875a58bb41295fc15b330255ffc3aa4493
Cliford <clifordjo@gmail.com>
2022-11-09 00:23:43 +0530

    added battery warnings to widget

--- .config/awesome/widgets/battery.lua
+++ .config/awesome/widgets/battery.lua
@@ -6,20 +6,17 @@
 
 local wibox = require("wibox");
 local upower   = require("lgi").require("UPowerGlib")
+local naughty = require("naughty")
 
 local battery_widget = {}
 
 function update_widget (device)
-	local perc = math.floor(device.percentage) .. "%"
 
-	if device.state == upower.DeviceState.PENDING_CHARGE or
-		device.state == upower.DeviceState.FULLY_CHARGED or
-		device.state == upower.DeviceState.CHARGING
-	then
-		perc = perc .. " A/C"
-	end
+	local is_charging = device.state == upower.DeviceState.PENDING_CHARGE or
+											device.state == upower.DeviceState.FULLY_CHARGED or
+											device.state == upower.DeviceState.CHARGING
 
-	battery_widget:update_battery(perc)
+	battery_widget:update_battery(math.floor(device.percentage), is_charging)
 end
 
 -- get current battery device
@@ -48,11 +45,24 @@ local worker = function(user_args)
 			widget = wibox.widget.textbox
 		},
 
-		update_battery = function(self, perc)
+		update_battery = function(self, percentage, is_charging)
+			local perc_str = percentage .. "%"
 
-			local battery_markup = string.format("<span font='%s' foreground='%s'>%s</span>", font, "#46fff6", perc);
+			if is_charging then
+				perc_str = perc_str .. " A/C"
+			end
+
+			local battery_markup = string.format("<span font='%s' foreground='%s'>%s</span>", font, "#46fff6", perc_str);
 
 			if self.percentage:get_markup() ~= battery_markup then
+				if percentage <= 10 and not is_charging then
+					naughty.notify({
+						title = "Battery Low",
+						text = "Battery level has dropped below 10%.\nShutdown imminent. Recharge immediately.",
+						icon = "/usr/share/icons/Adwaita/32x32/status/battery-level-10-charging-symbolic.symbolic.png",
+						timeout = 0
+					})
+				end
 				self.percentage:set_markup(battery_markup);
 			end
 		end
