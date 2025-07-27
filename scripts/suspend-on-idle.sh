8162a4be8259764212959dcf63486b3d2fee31e7
Cliford <clifordjo@gmail.com>
2025-07-27 09:51:03 +0530

    fix: don't hibernate on charging idle

--- scripts/suspend-on-idle.sh
+++ scripts/suspend-on-idle.sh
@@ -9,11 +9,15 @@ SLEEP_THRESHOLD_MINUTES=30
 SESSION_START_IDLE_FILE="/tmp/$$-session-idle-start"
 GRAPHICAL_USER="cliford"
 
+## SCRIPT
+
+trap "echo 'Shutting down idle watcher'; exit 0" SIGTERM SIGINT
+
 while true; do
 
-  pid=$(pidof awesome)
+  pid=$(pidof awesome || true)
 
-  if [ ! $? -eq 0 ]; then
+  if [ -z "$pid" ]; then
     # no graphical session started yet.
     # manually start timer for action
 
@@ -38,24 +42,29 @@ while true; do
       rm $SESSION_START_IDLE_FILE
     fi
 
-    # get the DISPLAY envvar
-    DISPLAY="$(grep -z ^DISPLAY /proc/$pid/environ | cut -z -d'=' -f2 | tr -d '\0' )"
-    idle_millis=$(DISPLAY=$DISPLAY sudo -u $GRAPHICAL_USER xprintidle 2>/dev/null )
+    display=$(grep -z ^DISPLAY /proc/$pid/environ | cut -z -d'=' -f2 | tr -d '\0')
+    idle_millis=$(DISPLAY=$display sudo -u $GRAPHICAL_USER xprintidle)
 
-    if [ $? -eq 0 ]; then
-      # successfully got idle time
-      idle_minutes=$((idle_millis / 60000))
-    else
-      echo "Failed to get idle millis from X session. DISPLAY was $DISPLAY"
+    if [ ! $? -eq 0 ]; then
+      echo "Failed to get idle millis from X session. DISPLAY was $display"
       continue
     fi
 
   fi
 
-  if [[ $idle_minutes -eq $SLEEP_THRESHOLD_MINUTES ]]; then
-    systemctl suspend-then-hibernate
+  idle_minutes=$((idle_millis / 60000))
+
+  if [[ $idle_minutes -ge $SLEEP_THRESHOLD_MINUTES ]]; then
+		echo "Caught lacking. Time to sleep"
+
+    charge_state=$(upower -i /org/freedesktop/UPower/devices/DisplayDevice | awk '/state/ {print $2}')
+
+    if [[ "$charge_state" == "charging" || "$charge_state" == "pending-charge"  || "$charge_state" == "fully-charged" ]]; then
+      systemctl suspend
+    else
+  		systemctl suspend-then-hibernate
+    fi
   fi
 
   sleep 60
-
 done
